90a43b0bb97c06ee492903bcd6961a9a89738119
epiccakeking <33770464+epiccakeking@users.noreply.github.com>
2019-11-10 11:11:29 -0800

    Update aopsenhanced.user.js

--- aopsenhanced.user.js
+++ aopsenhanced.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AoPS Enhanced
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  try to take over the world!
 // @author       happycupcake/EpicCakeKing
 // @match        https://artofproblemsolving.com/*
@@ -10,8 +10,6 @@
 
 (function() {
     'use strict';
-
-    // Your code here...
     //Enhanced quotes
     AoPS.Community.Views.Post.prototype.onClickQuote=function() {
         this.topic.appendToReply("[quote name=\"" +this.model.get("username") +"\" url=\"https://artofproblemsolving.com/community/p"+this.model.get("post_id")+"\"]\n" +this.model.get("post_canonical").trim()+"\n[/quote]\n\n");
@@ -47,4 +45,12 @@
     }
     //Disable idle monitor
     AoPS.Community.Constants.idle_monitor_interval=0;
+    //Add custom stylesheet
+    var sheet=document.createElement('style')
+    sheet.innerHTML=`
+#feed-topic .cmty-topic-moderate{
+display: inline !Important;
+}
+`
+    document.getElementsByTagName('head')[0].appendChild(sheet)
 })();
