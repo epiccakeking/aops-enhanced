20207deec64ae19c67e8b26c4728508c82a48868
epiccakeking <33770464+epiccakeking@users.noreply.github.com>
2019-12-03 22:45:07 -0800

    Update aopsenhanced.user.js

--- aopsenhanced.user.js
+++ aopsenhanced.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AoPS Enhanced
 // @namespace    http://tampermonkey.net/
-// @version      0.4.2
+// @version      0.5.1
 // @description  try to take over the world!
 // @author       happycupcake/EpicCakeKing
 // @match        https://artofproblemsolving.com/*
